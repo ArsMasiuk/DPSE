8442016878f00af275d707c31d5f31f2be21a931
AM <ars.masiuk@gmail.com>
2017-09-03 11:55:43 +0200

    scene options UI and IO

---
+++ dpss/src/qvge/CFileSerializerXGR.cpp
@@ -0,0 +1,46 @@
+﻿/*
+This file is a part of
+QVGE - Qt Visual Graph Editor
+
+(c) 2016 Ars L. Masiuk (ars.masiuk@gmail.com)
+
+It can be used freely, maintaining the information above.
+*/
+
+#include "CFileSerializerXGR.h"
+
+#include <QtCore/QFile>
+#include <QtCore/QDataStream>
+
+// reimp
+
+bool CFileSerializerXGR::load(const QString& fileName, CEditorScene& scene) const
+{
+	// read file into document
+	QFile file(fileName);
+	if (!file.open(QIODevice::ReadOnly))
+		return false;
+
+	scene.reset();
+
+	QDataStream ds(&openFile);
+	scene.restoreFrom(ds);
+	scene.readOptions(ds);
+
+	return true;
+}
+
+
+bool CFileSerializerXGR::save(const QString& fileName, const CEditorScene& scene) const
+{
+	QFile saveFile(fileName);
+	if (saveFile.open(QFile::WriteOnly))
+	{
+		QDataStream ds(&saveFile);
+		scene.storeTo(ds);
+
+		return true;
+	}
+
+	return false;
+}
